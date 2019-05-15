31d97da07c9aba68067837dac70dedfef6a02886
Shubham Verma <Shubham Verma@LAPTOP-6KSVJ6TI>
2019-05-15 22:06:27 +0530

    Updating the code.

--- AutomationPractice/Reports/cucumber-html-report/Report.html/report.js
+++ AutomationPractice/Reports/cucumber-html-report/Report.html/report.js
@@ -99,14 +99,14 @@ formatter.match({
   "location": "StepDefition.Open_browser()"
 });
 formatter.result({
-  "duration": 22869312300,
+  "duration": 26404451600,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_click_on_Sign_In_link()"
 });
 formatter.result({
-  "duration": 4347718000,
+  "duration": 4688418600,
   "status": "passed"
 });
 formatter.match({
@@ -123,95 +123,301 @@ formatter.match({
   "location": "StepDefition.User_enter_username_and_password(String,String)"
 });
 formatter.result({
-  "duration": 431023600,
+  "duration": 374368000,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.Click_on_SignIn_button()"
 });
 formatter.result({
-  "duration": 5155359400,
+  "duration": 5170445100,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_clicks_on_T_Shirts_tab()"
 });
 formatter.result({
-  "duration": 10903666500,
+  "duration": 5504848000,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_add_t_shirt_in_Cart()"
 });
 formatter.result({
-  "duration": 5207638000,
+  "duration": 5166004200,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button()"
 });
 formatter.result({
-  "duration": 10493139800,
+  "duration": 8887103800,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button_on_Summary_tab()"
 });
 formatter.result({
-  "duration": 5936013900,
+  "duration": 3356090700,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button_on_Address_tab()"
 });
 formatter.result({
-  "duration": 3227865500,
+  "duration": 2793379600,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_accepts_the_Terms_and_Condition()"
 });
 formatter.result({
-  "duration": 77537900,
+  "duration": 113338000,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_clicks_on_Proceed_to_checkout_button_on_Shipping_tab()"
 });
 formatter.result({
-  "duration": 5808439500,
+  "duration": 3247233400,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_choose_Pay_by_bank_wire_payment_option()"
 });
 formatter.result({
-  "duration": 2016433900,
+  "duration": 1873951500,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_confirms_the_order()"
 });
 formatter.result({
-  "duration": 8477754000,
+  "duration": 8256421300,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.Validate_order_has_been_done()"
 });
 formatter.result({
-  "duration": 56197000,
+  "duration": 51949700,
   "status": "passed"
 });
 formatter.match({
   "location": "StepDefition.User_clicks_on_back_to_order_history()"
 });
 formatter.result({
-  "duration": 3482039300,
+  "duration": 3985277400,
   "status": "passed"
 });
 formatter.after({
-  "duration": 162800,
+  "duration": 147000,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 164200,
+  "status": "passed"
+});
+formatter.uri("Update personal information.feature");
+formatter.feature({
+  "line": 1,
+  "name": "Update personal Information",
+  "description": "",
+  "id": "update-personal-information",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "line": 4,
+  "name": "Update personal information(First Name) in my account",
+  "description": "",
+  "id": "update-personal-information;update-personal-information(first-name)-in-my-account",
+  "type": "scenario",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "line": 3,
+      "name": "@Test"
+    }
+  ]
+});
+formatter.step({
+  "line": 5,
+  "name": "Open browser",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 6,
+  "name": "User click on Sign In link",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 7,
+  "name": "User enter username \"automationpractice25@gmail.com\" and password \"test@1234\"",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 8,
+  "name": "Click on SignIn button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "User clicks on My Personal Information button",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 10,
+  "name": "User enter \"newtestuserrrr\" in firstname input box",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 11,
+  "name": "User enters \"test@1234\" in Current password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 12,
+  "name": "User enters \"test@1234\" in New Password",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 13,
+  "name": "User enters \"test@1234\" in Confirmation",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 14,
+  "name": "Click on Save",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 15,
+  "name": "Validate message \"Your personal information has been successfully updated.\"",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "StepDefition.Open_browser()"
+});
+formatter.result({
+  "duration": 24740096000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.User_click_on_Sign_In_link()"
+});
+formatter.result({
+  "duration": 5173819000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "automationpractice25@gmail.com",
+      "offset": 21
+    },
+    {
+      "val": "test@1234",
+      "offset": 67
+    }
+  ],
+  "location": "StepDefition.User_enter_username_and_password(String,String)"
+});
+formatter.result({
+  "duration": 368651800,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefition.Click_on_SignIn_button()"
+});
+formatter.result({
+  "duration": 5254091100,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefitionUpdateProfile.User_clicks_on_My_Personal_Information_button()"
+});
+formatter.result({
+  "duration": 2506411200,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "newtestuserrrr",
+      "offset": 12
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enter_in_firstname_input_box(String)"
+});
+formatter.result({
+  "duration": 180737100,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test@1234",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enters_in_Current_password(String)"
+});
+formatter.result({
+  "duration": 124772000,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test@1234",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enters_in_New_Password(String)"
+});
+formatter.result({
+  "duration": 126849300,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "test@1234",
+      "offset": 13
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.User_enters_in_Confirmation(String)"
+});
+formatter.result({
+  "duration": 123741900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "StepDefitionUpdateProfile.Click_on_Save()"
+});
+formatter.result({
+  "duration": 2401538900,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "Your personal information has been successfully updated.",
+      "offset": 18
+    }
+  ],
+  "location": "StepDefitionUpdateProfile.Validate_message(String)"
+});
+formatter.result({
+  "duration": 58637200,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 21100,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 11700,
   "status": "passed"
 });
 });
